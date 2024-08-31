2ba3d89a608cc8671b36cbe0b39ea19443973a9c
Võ Viết Duy Khiêm <khiemmaychem@gmail.com>
2024-08-31 13:57:59 +0000

    profilev3

--- public/src/t/js/index.js
+++ public/src/t/js/index.js
@@ -30,22 +30,6 @@ let typed=new Typed(".GioiThieu", {
   typeSpeed: 95,
   backSpeed: 75,
   loop: true
-})
-// MUSIC PLAY
-const MusicList=[
-    "//thanhdieu.com/files/Em-Nào-Có-Tội.mp3",
-    "//thanhdieu.com/files/Anh-Đã-Quen-Với-Cô-Đơn.mp3",
-    "//thanhdieu.com/files/Về-Bên-Anh.mp3",
-];
-let t=parseInt(localStorage.getItem('td')) || Math.floor(Math.random() * MusicList.length);
-const audio=new Audio(MusicList[t]);
-function MusicPlay() {audio.play();}
-document.addEventListener('click', MusicPlay);
-audio.addEventListener("ended", function() {
-t=(t + 1) % MusicList.length;
-localStorage.setItem('td', t);
-audio.src=MusicList[t];
-audio.play();
 });
 // LOADER SCREEN ----------
 let spin=document.querySelector('.LoaderCover');
